29186672f0d7234f4c0977083a57e7e33355d77a
Björn Orri Sæmundsson <bjornorri@gmail.com>
2017-03-28 10:44:20 +0200

    Add menu item for enabling/disabling hotkeys

--- focus.lua
+++ focus.lua
@@ -27,13 +27,6 @@ local appkeys = {
 }
 
 
--- Bind hotkeys to apps.
-for key, app in pairs(appkeys) do
-    hs.hotkey.bind(hyper, key, function()
-        smartLaunchOrFocus(app)
-    end)
-end
-
 -- Launches or focuses the app.
 -- Cycles through windows if already focused.
 function smartLaunchOrFocus(appName)
@@ -53,3 +46,54 @@ function smartLaunchOrFocus(appName)
         hs.application.launchOrFocus(appName)
     end
 end
+
+-- Add hotkey menu item.
+local hotkeys = hs.menubar.new()
+hotkeys:setTitle("H")
+local settings = hs.settings.get("hotkeys") or {}
+-- Remove unwanted entries.
+for key, val in pairs(settings) do
+    if not contains(appkeys, key) then
+        settings[key] = nil
+    end
+end
+-- Add new entries.
+for key, val in pairs(appkeys) do
+    if settings[val] == nil then
+        settings[val] = true
+    end
+end
+
+-- Bind hotkeys to apps.
+function bindHotkeys()
+    for key, app in pairs(appkeys) do
+        if settings[app] == true then
+            hs.hotkey.bind(hyper, key, function()
+                smartLaunchOrFocus(app)
+            end)
+        else
+            hs.hotkey.deleteAll(hyper, key)
+        end
+    end
+end
+
+function populateMenu(key)
+    menuData = {}
+    for key, val in pairs(settings) do
+        local title = key
+        local checked = val
+        table.insert(menuData, 1, {title=title, checked=checked, fn=function()
+            -- Toggle hotkey.
+            settings[key] = not checked
+            hs.settings.set("hotkeys", settings)
+            bindHotkeys()
+        end})
+    end
+    -- Sort in alphabetical order.
+    local comp = function(a, b) return string.lower(a.title) < string.lower(b.title) end
+    table.sort(menuData, comp)
+    return menuData
+end
+bindHotkeys()
+hotkeys:setMenu(populateMenu)
+
