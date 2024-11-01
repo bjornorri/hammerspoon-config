9434eddd0732831da498c36313e1516f1d80cb71
Björn Saemundsson <bjornorri@gmail.com>
2024-11-01 13:03:23 +0100

    Don't use native MacOS window tiling by default

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ hs.loadSpoon("EmmyLua")
 require("hyper_key")
 require("reload_config")
 require("force_paste")
-require("native_windows")
+require("windows")
 require("app_focus")
 require("app_bundle_id_logger")
 
