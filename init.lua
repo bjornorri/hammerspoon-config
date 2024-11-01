ed0d4a161da547a6baff0401548906d16caa32d3
Björn Saemundsson <bjornorri@gmail.com>
2024-11-01 13:04:37 +0100

    Remove app bundle ID logger

--- init.lua
+++ init.lua
@@ -8,7 +8,6 @@ require("reload_config")
 require("force_paste")
 require("windows")
 require("app_focus")
-require("app_bundle_id_logger")
 
 --- Notify that the config was loaded.
 hs.notify.new({ title = "Hammerspoon", subTitle = "Config Loaded" }):autoWithdraw(true):send()
